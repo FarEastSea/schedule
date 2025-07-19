d2f562099d0a2e39e1b847c86c2903a0ca917b1f
FarEastSea <140769159+FarEastSea@users.noreply.github.com>
2025-07-19 13:22:07 +0800

    update something

--- program/UpdataUrl.py
+++ program/UpdataUrl.py
@@ -27,6 +27,8 @@ class VPN:
             except Exception as e:
                 print(f'出现错误：{e}')
                 continue
+            # 指定编码格式
+            res.encoding = res.apparent_encoding
             restext = res.text
             fileDir = os.path.dirname(os.path.dirname(__file__))
             filePath = os.path.join(fileDir, 'free')
